c835009ad0deba9611a82f9cf54d65784759d59a
freddy24-7 <freddy.austli@aol.com>
2022-04-12 15:51:24 +0200

    adding layout and buttons

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import { Switch, Route } from 'react-router-dom';
 
 import Layout from './components/Layout/Layout';
-import UserProfile from './components/Profile/UserProfile';
-import AuthPage from './pages/AuthPage';
 import HomePage from './pages/HomePage';
 
 function App() {
@@ -11,12 +9,6 @@ function App() {
       <Switch>
         <Route path='/' exact>
           <HomePage />
-        </Route>
-        <Route path='/auth'>
-          <AuthPage />
-        </Route>
-        <Route path='/profile'>
-          <UserProfile />
         </Route>
       </Switch>
     </Layout>
